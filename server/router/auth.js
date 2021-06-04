93e65771584b4f1018bed189a9d416c01c2777da
Nirajan Mahara <mnirajan9@gmail.com>
2021-06-04 14:24:35 +0545

    Storing the User Data in the Online Database using Express & Mongoose | Promises Version 🎇

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User = require('../model/userSchema');
+
 router.get('/', (req, res) => {
   res.send(`Hello from HOME router !!!`);
 });
 
 router.post('/register', (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
-  //   res.send(`Register Route !`)
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: 'Fill all fields properly !' });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: 'Email already exist !' });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: 'User registered successfully !' });
+        })
+        .catch((error) =>
+          res.status(501).json({ error: 'Failed to register !' })
+        );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
