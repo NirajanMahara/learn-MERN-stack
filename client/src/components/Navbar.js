0e8634537a7504d520b445494415f931d3b0d300
Nirajan Mahara <mnirajan9@gmail.com>
2021-06-05 15:52:28 +0545

    REACT-ROUTER-DOM | Open the New Page Without Reloading or Refresh the Page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-      <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-        <a class='navbar-brand' href='#'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <NavLink className='navbar-brand' to='#'>
           NAVBAR
-        </a>
+        </NavLink>
         <button
-          class='navbar-toggler'
+          className='navbar-toggler'
           type='button'
           data-toggle='collapse'
           data-target='#navbarSupportedContent'
@@ -17,35 +18,35 @@ const Navbar = () => {
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
-          <span class='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
 
-        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul class='navbar-nav ml-auto'>
-            <li class='nav-item active'>
-              <a class='nav-link' href='#'>
-                Home <span class='sr-only'>(current)</span>
-              </a>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item active'>
+              <NavLink className='nav-link' to='/'>
+                Home <span className='sr-only'>(current)</span>
+              </NavLink>
             </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='#'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/about'>
                 About
-              </a>
+              </NavLink>
             </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='#'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/contact'>
                 Contact
-              </a>
+              </NavLink>
             </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='#'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/login'>
                 Login
-              </a>
+              </NavLink>
             </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='#'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/register'>
                 Signup
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// using ES6 modules
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
